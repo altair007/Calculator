2a9e5eddf37e603d420edb3ed4f1056837c6d253
颜风 <1178328901@qq.com>
2014-05-25 10:21:16 +0800

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by   颜风 on 14-5-25.
+//  Copyright (c) 2014年 Shadow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
